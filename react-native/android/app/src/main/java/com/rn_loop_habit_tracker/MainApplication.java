17970196aa82fc66edbdf5995e927fa7b55ce907
Javier Artiles <javart@gmail.com>
2019-01-13 12:19:58 -0800

    wip

--- react-native/android/app/src/main/java/com/rn_loop_habit_tracker/MainApplication.java
+++ react-native/android/app/src/main/java/com/rn_loop_habit_tracker/MainApplication.java
@@ -3,6 +3,7 @@ package com.rn_loop_habit_tracker;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new ReactNativePushNotificationPackage(),
             new VectorIconsPackage()
       );
